09c194192d09f2d626bc277bb454a7983dd06aa9
Areddy28 <reddyanurag28@gmail.com>
2021-06-10 18:10:24 -0400

    Condition 1 Exercise

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let actualTemp = 70; 
+let desiredTemp = 60; 
+
+if (actualTemp > desiredTemp) {
+    console.log ("Run AC");
+} else if (actualtemp < desiredTemp) {
+    console.log ("Run Heat"); 
+} else {
+    console.log ("Stand By"); 
+}  
+
+
